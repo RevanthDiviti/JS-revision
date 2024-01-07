4bd22aa24dff649ba01d17f5e14b98553d4e6f0c
RevanthDiviti <145599691+RevanthDiviti@users.noreply.github.com>
2024-01-07 07:39:57 +0000

    01_Basics

---
+++ JS-Revision/01_Basics/01_Basics.js
@@ -0,0 +1,8 @@
+const accountID = 144553;
+let accountEmail = "revanth@gmail.com";
+var accountPassword = "12345";
+
+
+/*Prefer not to use var
+because of issue in block scope and functional scope
+*/
