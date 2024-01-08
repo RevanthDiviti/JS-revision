d87db326548972b2744953b6fde2ddc6468749cb
RevanthDiviti <145599691+RevanthDiviti@users.noreply.github.com>
2024-01-08 03:43:24 +0000

    Datatypes

---
+++ JS-Revision/01_Basics/02_DataTypes.js
@@ -0,0 +1,29 @@
+"use strict";  //treat all JS ode as newer version
+
+//alert("Hello") // We are using nodejs, not browser. So it will not work
+
+console.log(3
+    +3); //Code readability should be high
+console.log("Hitesh");
+
+let name = "Revanth"
+let age= 23
+let isLoggedIn = false
+let state;
+
+// -- Primitive Data Types --
+
+// number => 2 to power 53
+//bigint 
+// string =>" "
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//Object
+
+console.log(typeof "Revanth")  // String
+console.log(typeof age) //number
+console.log( typeof null); // object data type
+console.log(typeof undefined) //undefined
