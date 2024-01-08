d8e5cba69c6d7942da31236c72d457a0b038990e
RevanthDiviti <145599691+RevanthDiviti@users.noreply.github.com>
2024-01-08 05:00:04 +0000

    ConversionOperator

---
+++ JS-Revision/01_Basics/03_ConversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33"
+
+console.log(typeof score); //string
+console.log(typeof (score)); //string
+
+
+let valueInNumber = Number(score)  //number type
+
+let s= "33abc"
+let conv= Number(s) 
+console.log(typeof conv)   //NaN -Not a number
+
+let z = 0
+let co = Number(co)
+console.log(typeof co)  //NUmber
+console.log(co) // 0
+
+
+//When we convert values into boolean type
+//1 => true; 0=> false
+//""=>false
+//"hitesh" => true
+
+let someNumber = 33
+let stringNumber = string(someNumber)
+console.log(stringNumber); //33
+console.log(typeof stringNumber); //string
