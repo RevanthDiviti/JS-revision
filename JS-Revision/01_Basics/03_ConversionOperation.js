60ff9bb5ef953946bafbaf4c872184b077d9ff98
RevanthDiviti <145599691+RevanthDiviti@users.noreply.github.com>
2024-01-08 16:13:13 +0000

    Conversion Operation

--- JS-Revision/01_Basics/03_ConversionOperation.js
+++ JS-Revision/01_Basics/03_ConversionOperation.js
@@ -1,3 +1,5 @@
+
+/*
 let score = "33"
 
 console.log(typeof score); //string
@@ -14,14 +16,72 @@ let z = 0
 let co = Number(co)
 console.log(typeof co)  //NUmber
 console.log(co) // 0
-
+*/
 
 //When we convert values into boolean type
 //1 => true; 0=> false
 //""=>false
 //"hitesh" => true
 
+/*
 let someNumber = 33
 let stringNumber = string(someNumber)
 console.log(stringNumber); //33
 console.log(typeof stringNumber); //string
+*/
+
+//************************** Operations *****************
+
+/*
+
+let value = 3
+let negValue = -value
+console.log(negValue)  // -3
+
+*/
+
+// console.log("1"+2);  Ans- 12
+//console.log(1+"2"); Ans - 12
+//console.log("1"+2+2); Ans - 122
+//console.log(1+2+"2"); Ans- 32
+
+// From above examples, if we have string first then it treats everything as a string and it concatinates everything. If we have another data type then it treats everything in that datatype
+
+//Increment or Decrement operator
+
+/*
+
+let gameCounter = 100
+// gameCounter++;      // Ans - 101
+// ++gameCounter;      // Ans - 101
+console.log(gameChanger);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+const z = x++; Post increment
+console.log(`x:${x}, z:${z}`);
+
+let a = 3;
+const b = ++a; Pre increment
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+let r=5
+const e= r--; post decrement
+console.log(`"r": ${r},"e": ${e}`)
+// r: 4 e: 5
+
+const s =--r; ---Pre decrement
+console.log(`r:, ${r},s:, ${s}`)
+
+//r:3 s:3
+
+
+*/
+
