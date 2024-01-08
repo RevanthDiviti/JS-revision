c1435378431e82ebfe1badcbe92a9ba9c75db217
RevanthDiviti <145599691+RevanthDiviti@users.noreply.github.com>
2024-01-08 17:16:15 +0000

    Data Types Summery

---
+++ JS-Revision/01_Basics/Datatypes_Summery.js
@@ -0,0 +1,24 @@
+
+
+//Primitive - 7
+
+// String, Number, Boolean, null, undefined, symbol, BigInt
+
+
+
+//Reference (Non primitive)
+//Array, Objects, Functions
+
+
+//symbol - To make a value unique. Even the value is same it will not store in same memory location. It will store in different memory location
+
+const id = Symbol("123")
+const anotherID = Symbol("123")
+console.log(id, anotherID)
+console.log(typeof id , typeof anotherID)
+
+
+// Functions
+
+// 1) function() {}
+//2)Storing a function in a variable - "const myFunction = function(){} " 
