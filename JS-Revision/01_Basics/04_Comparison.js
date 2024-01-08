16d941811dd4fd20a753f84ada235bc22055d173
RevanthDiviti <145599691+RevanthDiviti@users.noreply.github.com>
2024-01-08 17:11:53 +0000

    Comparision and Data types Summery

---
+++ JS-Revision/01_Basics/04_Comparison.js
@@ -0,0 +1,4 @@
+// Comparision Operator
+
+console.log("2" ==2) // Ans- true => " == " - Compares only values and do not care about data types
+console.log("2" === 2) // Ans - false => "===" - compares both values and data types
